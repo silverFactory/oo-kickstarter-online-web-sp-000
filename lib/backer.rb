5771019c6919e3e46762f411c8e8eb8b84dd4db3
Will Studabaker <will.studabaker@gmail.com>
2020-11-03 15:42:44 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,6 +1,7 @@
 class Backer
-  attr_reader :backed_projects
-  def initialize
+  attr_reader :backed_projects :name
+  def initialize(name)
+    @name =name
     @backed_projects = []
   end
   def back_project(project)
